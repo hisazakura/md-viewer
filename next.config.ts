3095d27e68c536a581f924010afa478b5a5128bd
hisazakura <luthfanarrayyan@gmail.com>
2025-11-25 04:21:45 +0700

    prep for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const repo = "/md-viewer";
+
 const nextConfig: NextConfig = {
     output: "export",
+
+    basePath: repo,
+    assetPrefix: repo + "/",
+
+    images: {
+        unoptimized: true,
+    },
+
+    trailingSlash: true,
 };
 
 export default nextConfig;
