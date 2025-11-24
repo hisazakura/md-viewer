a7946ee1900f7bfbb65518c1f6ae0edcab4c28df
hisazakura <hisazakura@users.noreply.github.com>
2025-11-24 21:36:00 +0000

    deploy: c0d7bfd16a4bc77fa68eb03c0dd1e2345f86b373

---
+++ _next/static/RekkiT7lrt1pIG12nGCe8/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/md-viewer//_next/:path+",
+        "destination": "/md-viewer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
